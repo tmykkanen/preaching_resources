2f6c41111dbba2600733e361d102038d1fc294a3
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-05-14 12:18:21 -0500

    feat: :sparkles: added davis ch1 notes and corrected spacing in argument structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightAutoSidebar from "starlight-auto-sidebar";
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
